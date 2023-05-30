c5a9d18b4b15ed98b99765af5f1bae82829f1053
Tetiana299 <poliakovatv.29@gmail.com>
2023-05-30 22:52:13 +0300

    connected

--- google_sheets.py
+++ google_sheets.py
@@ -2,17 +2,23 @@ import telebot
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-def check_number():
-    pass
+# authorize 
+scope = ['https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive"]
 
-def go_to_menu():
-    pass
+credentials = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+client=gspread.authorize(credentials)
 
- 
-'''gs = gspread.service_account(filename='credentials.json')  # подключаем файл с ключами и пр.
-sh = gs.open_by_key('20431c324c801938ea77fd23e474c2e762fe66ac')  # подключаем таблицу по ID
-worksheet = sh.sheet1  # получаем первый лист
-result=worksheet.get_all_records()
 
-'''
+# connect to google sheets
+SHEET_LINK='1-0S0jFCpWo2gk_ejeedk86O5TaSNrhSZaEmIpGnlIzo'
+gs = gspread.service_account(filename='credentials.json')  # connect credentials
+sh = gs.open_by_key(SHEET_LINK)  # connect sheetID
+worksheet = sh.sheet1  # get 1st page
+
+result=worksheet.get_all_records() # get all rows from page
+
+print(result)
+
+
 
