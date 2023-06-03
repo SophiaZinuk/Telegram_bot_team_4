4de5b4e930b7a916ee074f98388e1047a79223ca
Tetiana299 <poliakovatv.29@gmail.com>
2023-06-03 10:45:18 +0300

    upd

--- google_sheets.py
+++ google_sheets.py
@@ -12,43 +12,44 @@ client=gspread.authorize(credentials)
 
 # connect to google sheets
 ID_TABLE='1-0S0jFCpWo2gk_ejeedk86O5TaSNrhSZaEmIpGnlIzo'
-sh = client.open_by_key(ID_TABLE)  # connect sheetID
-
-### Registration /
-db_tenants = sh.sheet1  # def 1st sheet - tenant_database
-ids_telegram=sh.worksheet('Лист2') # def 2nd sheet
-
-
 LEN_OF_NUMBER=(9,13)
 
+db= client.open_by_key(ID_TABLE)  # connect sheetID
+
+db_sheets=tuple(db.worksheets())
+
+
+### Registration /
+
 # check correct telephone number
-def is_correct_number(message:str)->int:
+def is_correct_number(message:str)->str:
     
     message=''.join(message.strip().split())
 
     #check and return last 10 digits of number
     if len(message) in range(*LEN_OF_NUMBER) and message[-10:].isdigit():
-        return int(message.strip()[-10:])
+        return message.strip()[-10:]
     else:
-        return -1
+        return 'Enter correct number!'
     
 
 # check number of telephon in tenant_database
-def is_in_db_tenants( tel_number:int )->bool:
-    
-    for row in db_tenants.get_all_records():       
-       if row['telephon']%10**10==tel_number:
-           return True
+def is_in_db_tenants( tel_number:str )->bool:
+    tel_nums=db_sheets[0].col_values(col=3)
+    for i in range(1, len(tel_nums)):       
+        if tel_number in tel_nums[i]:
+            return True 
     return False
 
 # is tel in db_tenants to add id_user_telegram and tel
 def add_user_id(user_id, telephone)->bool:
-
-    if user_id not in ids_telegram.col_values(col=1):
-        ids_telegram.append_row(user_id, telephone)
+    users_telegram=db_sheets[1]
+    
+    if user_id not in users_telegram.col_values(col=1):
+        users_telegram.append_row(values=(user_id, telephone))
         return True
-    else: 
-        return False
+    
+    return False
 
 ### /end Registration
 
@@ -56,15 +57,17 @@ def add_user_id(user_id, telephone)->bool:
 
 
 #Tests 
-'''
-values=('253648', '56485235458')
+
+values=('236248', '56253525524458')
 print(add_user_id(*values))
-print(ids_telegram.get_all_records())
 
 
-tel='380664442233'
-print(is_correct_number(tel))
-#print( is_in_db_tenants(tel))
 
-'''
+tel='380664442243'
+t=is_correct_number(tel)
+print(t)
+if t!=-1:
+    print(is_in_db_tenants(t))
+
+
 
